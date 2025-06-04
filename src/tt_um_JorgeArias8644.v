be7f1e996e8d1ecd3d08d007293b56628b02d21f
Jorge <jariasm@miumg.edu.gt>
2025-06-03 23:22:41 -0600

    Update tt_um_JorgeArias8644.v

--- src/tt_um_JorgeArias8644.v
+++ src/tt_um_JorgeArias8644.v
@@ -5,7 +5,7 @@ input  [7:0] ui_in,
 //input  [7:0] ui_in,
 // input [9:8] ui_in,
 output [7:0] uo_out,
- /*input  [7:0] uio_in,*/
+input  [2:0] uio_in,
  /*output [7:0] uio_out,*/
  /*output [7:0] uio_oe,*/
 input clk,

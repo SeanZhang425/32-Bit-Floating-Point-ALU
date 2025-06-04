6323c966f6d73654529a10bcff05957be25f9dac
Jorge <jariasm@miumg.edu.gt>
2025-06-03 22:56:06 -0600

    alu_1bit.v

--- src/alu_1bit.v
+++ src/alu_1bit.v
@@ -1,7 +1,7 @@
 module alu_1bit(
                 input  at,bt,
                 input  [1:0]ALUcontrol,
-                output result,Z,N,C,V
+                output result
                );
            wire int1,int2,int3,sum_out,cout_sum,mym;
             reg n;
@@ -17,11 +17,11 @@ module alu_1bit(
    // Instanciando multiplexor de 4 entradas a 1 salida con selector de 2 bits
    mux4 mux4_1(.d0(sum_out),.d1(sum_out),.d2(int1),.d3(int2),.mux4_out(result),.s(ALUcontrol));
    // Instanciando compuerta XOR de 3 entradas y 1 salida negativa
-   xor_3a1n xor31 (.a1(at),.a2(bt),.a3(ALUcontrol[0]),.y2(V));
+ //  xor_3a1n xor31 (.a1(at),.a2(bt),.a3(ALUcontrol[0]),.y2(V));
     
-    always @* begin 
+/*    always @* begin 
        Z = ~& result;
        C = ~ALUcontrol[1] & cout_sum;
     end
-                
+*/                
 endmodule

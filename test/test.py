efcad71093c8295384ae175efc00b4aa06b4ae62
Jorge <jariasm@miumg.edu.gt>
2025-06-03 21:16:14 -0600

    test.py

--- test/test.py
+++ test/test.py
@@ -1,18 +1,38 @@
-import cocotb
-from cocotb.triggers import Timer
+`default_nettype none
+`timescale 1ns / 1ps
 
-@cocotb.test()
-async def test_basic_addition(dut):
-    # S = 00 (suma), a = 1, b = 2
-    S = 00
-    a = 1
-    b = 2
-#codificar en io_in: [7:0]=a [7:0]=b [1:0]=S
-dut.ui_in.value = (sel << 3) | (a << 2) |b
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
 
-await Timer(10,units='ns')
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-expected = a + b
-actual = dut.uo_out.value.integer
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-assert actual == expected, f"Suma fallida: esperando {expected}, obtenido {actual}"
+  // Replace tt_um_example with your module name:
+  tt_um_example user_project (
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
+
+endmodule

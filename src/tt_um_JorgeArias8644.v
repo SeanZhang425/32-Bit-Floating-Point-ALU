4b15a91d6ba29a2529d06a5027367e7765275dfe
Jorge <jariasm@miumg.edu.gt>
2025-06-03 21:49:20 -0600

    tt_um_JorgeArias8644.v

--- src/tt_um_JorgeArias8644.v
+++ src/tt_um_JorgeArias8644.v
@@ -1,8 +1,8 @@
 module tt_um_JorgeArias8644(
 
 input  [7:0] ui_in,
-//input  [7:0] ui_in,
-input  [2:0] ui_in,
+input  [7:0] ui_in,
+ input [9:8] ui_in,
 output [7:0] uo_out,
 input clk,
 input ena,

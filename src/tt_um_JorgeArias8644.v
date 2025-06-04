96700cf8c4a3abbe11fd8d8e2c171eb0d0dfdbaa
Jorge <jariasm@miumg.edu.gt>
2025-06-03 20:54:52 -0600

    tt_um_JorgeArias8644.v

--- src/tt_um_JorgeArias8644.v
+++ src/tt_um_JorgeArias8644.v
@@ -1,9 +1,9 @@
 module tt_um_JorgeArias8644(
 
 input  [7:0] ui_in,
+input  [7:0] ui_in,
+input  [2:0] ui_in,
 output [7:0] uo_out,
-input  [7:0] uio_in,
-output [7:0] uio_oe,
 input clk,
 input ena,
 input rst_n
@@ -11,10 +11,10 @@ input rst_n
 );
 
  // Declaración de señales
- reg [7:0] a;
- reg [7:0] b;
- reg [1:0] S;
- wire [7:0] Result;
+ reg [7:0] a       =  ui_in[7:0]; // Input variable A de 8 bits
+ reg [7:0] b       =  ui_in[7:0]; // Input variable B de 8 bits
+ reg [1:0] S       =  ui_in[2:0]; // Input selector de operación 
+ wire [7:0] Result = uo_out[7:0]; // Output result operations
 
  // Instancia del módulo alu_8bits
  alu_8bits alu_inst (
